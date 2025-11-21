cba672425bc1a57390856440467948acbd43691a
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2025-11-21 15:34:13 -0500

    implemented initial styles for header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,13 +6,13 @@ function Header() {
   return (
     <header className='header'>
 		<div className='header-div-logo'>
-			<img></img> test
+			<img className='header-div-logo-img' src='https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/new-website%2FLogo-small.webp?alt=media&token=767ffa3c-9937-4722-bc94-42b06612f261'></img> test
 		</div>
 		<div className='header-div-links'>
-		
+			
 		</div>
 		<div className='header-div-socials'>
-		
+			
 		</div>
     </header>
   )
