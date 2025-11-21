cd76078f3b10ca576d5e001291a45da594aa0f51
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2025-11-21 10:26:26 -0500

    made progress on header

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './header.scss'
+
+function Header() {
+
+  return (
+    <header className='header'>
+		<div className='header-div-logo'>
+			<img></img> test
+		</div>
+		<div className='header-div-links'>
+		
+		</div>
+		<div className='header-div-socials'>
+		
+		</div>
+    </header>
+  )
+}
+
+export default Header;
