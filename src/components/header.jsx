6dcf27ded4d14afa828316c27228f3533ad98761
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2025-11-21 15:47:56 -0500

    updated favicon, made more changes to style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,10 +6,12 @@ function Header() {
   return (
     <header className='header'>
 		<div className='header-div-logo'>
-			<img className='header-div-logo-img' src='https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/new-website%2FLogo-small.webp?alt=media&token=767ffa3c-9937-4722-bc94-42b06612f261'></img> test
+			<img className='header-div-logo-img' src='https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/new-website%2FLogo-small.webp?alt=media&token=767ffa3c-9937-4722-bc94-42b06612f261'></img>
 		</div>
 		<div className='header-div-links'>
-			
+			<span>Résumé</span>
+			<span>Portfolio</span>
+			<span>Contact</span>
 		</div>
 		<div className='header-div-socials'>
 			
