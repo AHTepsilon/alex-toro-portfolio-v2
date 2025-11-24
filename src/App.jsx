1e52136224d1513df20f61b657873951ea7d5dcd
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2025-11-24 14:27:38 -0500

    Made progress on landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import Header from './components/header.jsx'
 
 function App() {
@@ -7,8 +7,13 @@ function App() {
   return (
     <>
 	<Header></Header>
+	<section className="landing-section">
+		<div className="landing-section-div">
+			<img className="landing-section-div-img" src="https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/new-website%2Fsection-logo.svg?alt=media&token=f5ba2a4b-bee4-4491-82bb-9d956b3c5a6b"/>
+		</div>
+	</section>
     </>
-  )
+  );
 }
 
 export default App;
