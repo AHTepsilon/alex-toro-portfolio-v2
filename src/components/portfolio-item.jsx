e0222c710cd074611e69d0622bf80bb94ac0e109
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2026-01-26 15:51:36 -0500

    Improved portfolio section

--- src/components/portfolio-item.jsx
+++ src/components/portfolio-item.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
-import './footer.scss';
+import './portfolio-item.scss';
 import './footer-media-queries.scss'
 import { NavLink } from "react-router";
 
-function PortfolioItem({thumbnail, description}) {
+function PortfolioItem({thumbnail, description, link}) {
 
   return (
-    <div className="portfolio-item-div">
-		<img className="portfolio-item-div-img" src={thumbnail}></img>
+	<NavLink className="portfolio-item-div" to={link} target="_blank">
 		<div className="portfolio-item-div-inner">
+			<img className="portfolio-item-div-inner-img" src={thumbnail}></img>
 			<p className="portfolio-item-div-inner-text">{description}</p>
 		</div>
-	</div>
+	</NavLink>
   )
 }
 
--- src/pages/TRS-page.jsx
+++ src/pages/TRS-page.jsx
@@ -11,7 +11,8 @@ function TRS() {
 	<>
 		<Header></Header>
 			<section className = "main-container">
-				<PortfolioItem thumbnail="https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/Portfolio-Pics%2FTRS%2FPortfolio-TRS-1.jpg?alt=media&token=beea1d37-5350-41ab-9477-93958985d2c0" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></PortfolioItem>
+				<h2 className="main-container-title">Tu Respaldo Seguro S.A.S.</h2>
+				<PortfolioItem thumbnail="https://firebasestorage.googleapis.com/v0/b/alextorodesigns.appspot.com/o/Portfolio-Pics%2FTRS%2FPortfolio-TRS-1.jpg?alt=media&token=beea1d37-5350-41ab-9477-93958985d2c0" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." link="https://www.instagram.com/p/DFbINKnJFdA/?img_index=1"></PortfolioItem>
 			</section>
 		<Footer></Footer>
 	</>
