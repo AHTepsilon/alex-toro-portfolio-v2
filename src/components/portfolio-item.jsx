fe6bac0238a9d71ec4b8b43c951a13e23bf743e7
ALEJANDRO HERNANDEZ TORO <alehertoro725@gmail.com>
2026-01-27 15:31:30 -0500

    Implemented changes related to how tools were visualized in portfolio items

--- src/components/portfolio-item.jsx
+++ src/components/portfolio-item.jsx
@@ -4,15 +4,16 @@ import './portfolio-item-media-queries.scss';
 import './footer-media-queries.scss'
 import { NavLink } from "react-router";
 
-function PortfolioItem({thumbnail, description, link, section}) {
+function PortfolioItem({thumbnail, description, link, section, tools}) {
 
   return (
-	<NavLink className="portfolio-item-div fade-in" to={link} target="_blank">
+	<NavLink className="portfolio-item-div fade-in" to={link} target="_blank" id="portfolio-item">
 		<div className="portfolio-item-div-inner">
 			<img className="portfolio-item-div-inner-img" src={thumbnail}></img>
 			<div className="portfolio-item-div-inner-textarea">
 				<p className="portfolio-item-div-inner-textarea-text">{description}</p>
 				<p className="portfolio-item-div-inner-textarea-typeof">{section}</p>
+				<p className="portfolio-item-div-inner-textarea-tools">{tools}</p>
 			</div>
 		</div>
 	</NavLink>
